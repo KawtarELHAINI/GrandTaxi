bec8cbecbc93d92fd425ac8d47488ca1484afaeb
KawtarELHAINI <kawtarelhaini3@gmail.com>
2024-02-27 10:08:09 +0100

    V-1.2

---
+++ database/migrations/2024_01_29_132823_create_recettes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('publication', function (Blueprint $table) {
+            $table->id();
+            $table->integer('date');
+            $table->text('body');
+            $table->string('lieu1');
+            $table->string('lieu2');
+            $table->string('prix');  
+            $table->softDeletes();
+            $table->timestamps();
+            $table->unsignedBigInteger('profile_id');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('publication');
+    }
+};
